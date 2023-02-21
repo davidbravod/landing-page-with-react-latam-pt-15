d3c42b903afa78550f5128cd5282880d4f7e3dc9
davidbravod <bravod95@gmail.com>
2023-02-21 05:17:42 +0000

    final update for the react project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,24 +2,55 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar.jsx"
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx"
+import Footer from "./footer.jsx"
+
+
+
 
 //create your first component
 const Home = () => {
+	let tituloNoticias = [
+		{
+		  titulo: "LA IA",
+		  noticia: "La IA dominará el mundo",
+		},
+		{
+		  titulo: "El Mundo",
+		  noticia: "El mundo es grande",
+		},
+		{
+		  titulo: "Turquía",
+		  noticia: "Sismo en Turquía",
+		},
+		{
+			titulo: "David Bravo",
+			noticia: "Está programando en React",
+		  },
+	  ];
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<Navbar/>
+		<Jumbotron/>
+		<div className="album py-3">
+			<div className="container">
+				<div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+				{tituloNoticias.map((item, index) => {
+        return (
+          <Card
+            titulo={item.titulo}
+            descripcion={item.noticia}
+			key={index}
+          />
+        );
+      })}
+				</div>
+			</div>
 		</div>
+		<Footer/>
+		</>
 	);
 };
 
